585a4244f127d8f4631efd8322f20bf08840cecc
Piero <piero@devls.pl>
2024-12-17 20:12:17 +0100

    day 17 part 1 instructions inside data

--- 17/part_one.hs
+++ 17/part_one.hs
@@ -1,17 +1,17 @@
-import Data.Array (array, (!))
+import Data.Array (Array, array, (!))
 import Data.Bits (xor)
 import Data.Char (isDigit)
 
 -- import Debug.Trace (trace)
 
-data VM = VM {ip :: Int, ar :: Int, br :: Int, cr :: Int, outp :: [Int], code :: [Int]} deriving (Show)
+data VM = VM {ip :: Int, ar :: Int, br :: Int, cr :: Int, outp :: [Int], code :: [Int], inst :: Array Int (Int -> VM -> VM)}
 
-initVm ini vm =
+initVm ini inst vm =
   let ar = read (ini !! 0)
       br = read (ini !! 1)
       cr = read (ini !! 2)
       code = filter (>= 0) $ map (\x -> if isDigit x then read [x] :: Int else -1) $ last ini
-   in VM {ip = 0, ar = ar, br = br, cr = cr, outp = [], code = code}
+   in VM {ip = 0, ar = ar, br = br, cr = cr, outp = [], code = code, inst = inst}
 
 -- instructions helpers
 
@@ -46,13 +46,13 @@ cdv x vm = vm {cr = dv x vm}
 
 -- runtime :)
 
-exec (ins, op) vm insAr = (insAr ! ins) op vm {ip = ip vm + 2}
+exec (ins, op) vm = (inst vm ! ins) op vm {ip = ip vm + 2}
 
-runCmd [] vm _ = vm {ip = -1}
-runCmd [a, b] vm insAr = exec (a, b) vm insAr
+runCmd [] vm = vm {ip = -1}
+runCmd [a, b] vm = exec (a, b) vm
 
 -- runVm insAr vm = trace ("cmd " ++ show nextCmd ++ " ip " ++ show (ip vm) ++ "\n" ++ show vm) (runCmd nextCmd vm insAr)
-runVm insAr vm = runCmd nextCmd vm insAr
+runVm vm = runCmd nextCmd vm
   where
     nextCmd = take 2 $ drop (ip vm) (code vm)
 
@@ -60,7 +60,7 @@ main = do
   -- txt <- readFile "../data/2024/17/test_input"
   txt <- readFile "../data/2024/17/input"
   let ini = map (dropWhile (not . isDigit)) $ lines txt
-  let vm = initVm ini VM
   let insAr = array (0, 7) [(0, adv), (1, bxl), (2, bst), (3, jnz), (4, bxc), (5, out), (6, bdv), (7, cdv)]
-  let r = outp $ until (\vm -> ip vm < 0) (runVm insAr) vm
+  let vm = initVm ini insAr VM
+  let r = outp $ until (\vm -> ip vm < 0) runVm vm
   print r
